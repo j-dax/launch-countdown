84b7fb69682a95db55e949fb99b7bbf9da8346b9
j-dax <67487509+j-dax@users.noreply.github.com>
2024-03-23 16:15:05 -0700

    ... saving progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,120 @@
+'use client';
+
 import Image from "next/image";
 
+import dayjs from "dayjs"
+import duration from "dayjs/plugin/duration"
+dayjs.extend(duration)
+import relativeTime from "dayjs/plugin/relativeTime"
+dayjs.extend(relativeTime)
+
+import bgStars from "@/public/images/bg-stars.svg"
+import patternHills from "@/public/images/pattern-hills.svg"
+import iconFb from "@/public/images/icon-facebook.svg"
+import iconIg from "@/public/images/icon-instagram.svg"
+import iconPt from "@/public/images/icon-pinterest.svg"
+
+function Hills() {
+  return <div className="absolute bottom-0 min-w-full">
+    <Image
+      alt="background layer: hills"
+      src={patternHills}
+      className="min-w-full"
+    />
+    <nav className="absolute min-w-full flex gap-20 justify-center bottom-16">
+      <Image
+        alt="icon linking to facebook"
+        src={iconFb}
+        className=""
+      />
+      <Image
+        alt="icon linking to Instagram"
+        src={iconIg}
+        className=""
+      />
+      <Image
+        alt="icon linking to Pinterest"
+        src={iconPt}
+        className=""
+      />
+    </nav>
+  </div>
+}
+
+function Stars() {
+  return <div className="min-w-full min-h-full">
+    <Image
+      alt="background layer: stars"
+      src={bgStars}
+      className="min-w-full absolute left-0 top-0"
+    />
+  </div>
+}
+
+type CountdownSplitProps = {
+  descriptor: string
+  n: number
+}
+
+function CountdownSplit({n, descriptor}: CountdownSplitProps) {
+  const num = `${n}`.padStart(2, '0')
+  return <div className="min-w-24">
+    <div className="absolute flex gap-1 bg-countdown rounded-md p-4">
+      <p className="relative text-center text-countdown-timer font-bold text-6xl text-red-400">{num}</p>
+      <p className="absolute text-center text-countdown-timer font-bold text-6xl text-red-400">{num}</p>
+    </div>
+    <p className="text-center text-countdown-desc">{descriptor}</p>
+  </div>
+}
+
+type CountdownProps = {
+  end: dayjs.Dayjs
+}
+
+import {useState, useEffect} from "react"
+function CountdownTimer({end}: CountdownProps) {
+  const update = (now: number) => {
+    setDays(end.diff(now, 'days'));
+    setHours(end.diff(now, 'hours') % 24);
+    setMinutes(end.diff(now, 'minutes') % 60);
+    setSeconds(end.diff(now, 'seconds') % 60);
+  }
+
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  // update the timer every second
+  useEffect(() => {
+    update(Date.now());
+    setTimeout(() => {
+      const now = Date.now();
+      update(now);
+    }, 1000);
+  });
+
+  return <>
+    <CountdownSplit n={days} descriptor="days" />
+    <CountdownSplit n={hours} descriptor="hours" />
+    <CountdownSplit n={minutes} descriptor="minutes" />
+    <CountdownSplit n={seconds} descriptor="seconds" />
+  </>
+}
+
 export default function Home() {
+  const dur = dayjs.duration({'days': 10});
+  const endTime = dayjs(new Date()).add(dur);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="bg-gradient-to-b from-space to-ground min-h-screen">
+      <Stars/>
+      <div className="absolute min-w-full min-h-full flex flex-col justify-center items-center gap-8">
+        <p className="mb-16 font-bold">We're launching soon</p>
+        <div className="flex gap-8 mb-32">
+          <CountdownTimer end={endTime} />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <Hills/>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,14 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      "space": "#241e2c",
+      "ground": "#2e1928",
+      "countdown": "#2c2c44",
+      "countdown-desc": "#817d94",
+      "countdown-drop": "#191923",
+      "countdown-timer": "#fc6085",
+    },
   },
   plugins: [],
 };
