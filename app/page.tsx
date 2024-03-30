f78eb5bfc4504c7a3129264c222b0a8c6d712e4d
j-dax <67487509+j-dax@users.noreply.github.com>
2024-03-30 16:29:28 -0700

    feat(countdown): incomplete, needs unified transition

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import {useState, useEffect, useRef} from "react"
+
 import Image from "next/image";
 
 import dayjs from "dayjs"
@@ -54,16 +56,34 @@ function Stars() {
 type CountdownSplitProps = {
   descriptor: string
   n: number
+  doUpdate: boolean
 }
 
-function CountdownSplit({n, descriptor}: CountdownSplitProps) {
-  const num = `${n}`.padStart(2, '0')
-  return <div className="min-w-24">
-    <div className="absolute flex gap-1 bg-countdown rounded-md p-4">
-      <p className="relative text-center text-countdown-timer font-bold text-6xl text-red-400">{num}</p>
-      <p className="absolute text-center text-countdown-timer font-bold text-6xl text-red-400">{num}</p>
+function CountdownSplit({n, descriptor, doUpdate }: CountdownSplitProps) {
+  const num = `${n}`.padStart(2, '0');
+  const nextNum = `${n-1}`.padStart(2, '0');
+  const doWiggle = doUpdate ? "animate-fall" : "";
+  const doDelayed = doUpdate ? "delay-300 animate-fall" : "";
+  return <div className="flex flex-col">
+    <div className="relative flex flex-col bg-cd-drop max-w-32 max-h-[132px] rounded-md">
+      {/* The rear number, seen when the middle element folds down */}
+      <div className="relative flex items-center gap-1 min-w-32 min-h-32 rounded-md bg-cd-top">
+        <p className="mx-auto text-center text-countdown-timer font-bold text-6xl text-cd-top-text">{nextNum}</p>
+      </div>
+      {/* The folding upper half, text should become hidden after 50% folded */}
+      <div className={`relative flex items-center gap-1 min-w-32 min-h-32 rounded-md hide-bottom top-[-8rem] bg-cd-top border-space border-2 ${doWiggle}`}>
+        <p className="mx-auto text-center text-countdown-timer font-bold text-6xl text-cd-top-text">{num}</p>
+      </div>
+      {/* The bottom half */}
+      <div className={`relative flex items-center gap-1 min-w-32 min-h-32 rounded-md hide-top top-[-16rem] bg-cd-bottom origin-bottom ${doDelayed}`}>
+        <p className="mx-auto text-center text-countdown-timer font-bold text-6xl text-cd-bottom-text">{num}</p>
+      </div>
+
+      {/* Two semicircles, as if carving out part of the countdown */}
+      <div className="relative min-w-2 min-h-2 max-w-2 max-h-2 top-[-324px] left-[-4px] rounded-full bg-cd-drop hide-left" />
+      <div className="relative min-w-2 min-h-2 max-w-2 max-h-2 top-[-332px] left-[124px] rounded-full bg-cd-drop hide-right" />
     </div>
-    <p className="text-center text-countdown-desc">{descriptor}</p>
+    <p className="relative text-center font-bold text-cd-bottom-text">{descriptor}</p>
   </div>
 }
 
@@ -71,45 +91,55 @@ type CountdownProps = {
   end: dayjs.Dayjs
 }
 
-import {useState, useEffect} from "react"
 function CountdownTimer({end}: CountdownProps) {
-  const update = (now: number) => {
+  const update = () => {
+    setNow(dayjs(Date.now()));
     setDays(end.diff(now, 'days'));
     setHours(end.diff(now, 'hours') % 24);
     setMinutes(end.diff(now, 'minutes') % 60);
     setSeconds(end.diff(now, 'seconds') % 60);
   }
 
-  const [days, setDays] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
+  const [now, setNow] = useState(dayjs(Date.now()));
+  const [days, setDays] = useState(end.diff(now, 'days'));
+  const [hours, setHours] = useState(end.diff(now, 'hours') % 24);
+  const [minutes, setMinutes] = useState(end.diff(now, 'minutes') % 60);
+  const [seconds, setSeconds] = useState(end.diff(now, 'seconds') % 60);
 
   // update the timer every second
   useEffect(() => {
-    update(Date.now());
-    setTimeout(() => {
-      const now = Date.now();
-      update(now);
-    }, 1000);
+    update();
+    setTimeout(update, 1000);
   });
 
   return <>
-    <CountdownSplit n={days} descriptor="days" />
-    <CountdownSplit n={hours} descriptor="hours" />
-    <CountdownSplit n={minutes} descriptor="minutes" />
-    <CountdownSplit n={seconds} descriptor="seconds" />
+    <CountdownSplit
+      n={Math.max(days)}
+      descriptor={"days".toUpperCase()}
+      doUpdate={(days > 0 && hours == 0)} />
+    <CountdownSplit 
+      n={Math.max(hours, 0)}
+      descriptor={"hours".toUpperCase()} 
+      doUpdate={(days > 0 || hours > 0) && minutes == 0} />
+    <CountdownSplit 
+      n={Math.max(minutes, 0)}
+      descriptor={"minutes".toUpperCase()}
+      doUpdate={(hours > 0 || minutes > 0) && seconds == 0} />
+    <CountdownSplit 
+      n={Math.max(seconds, 0)} 
+      descriptor={"seconds".toUpperCase()}
+      doUpdate={seconds != 0 && end.isAfter(now)} />
   </>
 }
 
 export default function Home() {
-  const dur = dayjs.duration({'days': 10});
+  const dur = dayjs.duration({'seconds': 5});
   const endTime = dayjs(new Date()).add(dur);
   return (
-    <div className="bg-gradient-to-b from-space to-ground min-h-screen">
+    <div className="bg-gradient-to-b from-space to-ground min-h-full overflow-hidden">
       <Stars/>
       <div className="absolute min-w-full min-h-full flex flex-col justify-center items-center gap-8">
-        <p className="mb-16 font-bold">We're launching soon</p>
+        <p className="mb-16 font-bold text-cd-top-text">We're launching soon</p>
         <div className="flex gap-8 mb-32">
           <CountdownTimer end={endTime} />
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,14 +13,30 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        'wiggle': {
+          '0%': { transform: 'rotateX(0deg)' },
+          '50%': { transform: 'rotateX(90deg)' },
+          '100%': { transform: 'rotateX(180deg)' },
+        },
+        'fall': {
+          '0%': { transform: 'rotateX(0deg)' },
+          '100%': { transform: 'rotateX(90deg)' },
+        },
+      },
+    },
+    animation: {
+      'wiggle': 'wiggle 500ms linear infinite',
+      'fall': 'fall 500ms linear infinite',
     },
     colors: {
       "space": "#241e2c",
       "ground": "#2e1928",
-      "countdown": "#2c2c44",
-      "countdown-desc": "#817d94",
-      "countdown-drop": "#191923",
-      "countdown-timer": "#fc6085",
+      "cd-top": "#2c2c44",
+      "cd-top-text": "#d35070",
+      "cd-bottom": "#323651",
+      "cd-bottom-text": "#fa5e85",
+      "cd-drop": "#1a1921"
     },
   },
   plugins: [],
